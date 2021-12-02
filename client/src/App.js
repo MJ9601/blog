53eea5b7edb77d716af810ae682135d6f4d7312e
mj khodadadi <m_javad_96@yahoo.com>
2021-12-01 17:26:02 -0800

    replacing

---
+++ client/src/App.js
@@ -0,0 +1,106 @@
+import Navbar from "./components/navbar/Navbar";
+import CreatePost from "./components/pages/create_post/CreatePost";
+import Home from "./components/pages/home/Home";
+import Home_page from "./components/pages/home_page/Home_page";
+import Login from "./components/pages/login/Login";
+import PostPage from "./components/pages/post_page/PostPage";
+import UnregisteredSetting from "./components/pages/setting-update/UnregisteredSetting";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import SettingSec from "./components/pages/setting-update/setting-sec/SettingSec";
+import { useState } from "react";
+
+function App() {
+  const [User, setUser] = useState(false);
+  return (
+    <Router>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            !User ? (
+              <>
+                <Navbar setUser={setUser} User={User} /> <Home_page />
+              </>
+            ) : (
+              <>
+                <Navbar /> <Home />
+              </>
+            )
+          }
+        />
+        <Route
+          path="/Login"
+          element={
+            !User && (
+              <>
+                <Login />
+              </>
+            )
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            !User && (
+              <>
+                <Navbar setUser={setUser} User={User} /> <UnregisteredSetting />
+              </>
+            )
+          }
+        />
+        <Route
+          path="/:userName/profile"
+          element={
+            User ? (
+              <>
+                <Navbar setUser={setUser} User={User} /> <Home />
+              </>
+            ) : (
+              <>
+                <Navbar setUser={setUser} User={User} /> <Home />
+              </>
+            )
+          }
+        />
+        <Route
+          path="/:userName/setting"
+          element={
+            User && (
+              <>
+                <Navbar setUser={setUser} User={User} /> <SettingSec />
+              </>
+            )
+          }
+        />
+        <Route
+          path="/:userName/createPost"
+          element={
+            User && (
+              <>
+                <Navbar setUser={setUser} User={User} /> <CreatePost />
+              </>
+            )
+          }
+        />
+        <Route
+          path="/:userName/showPost/:PostId"
+          element={
+            User ? (
+              <>
+                <Navbar setUser={setUser} User={User} /> <PostPage />
+              </>
+            ) : (
+              <>
+                <Navbar setUser={setUser} User={User} /> <PostPage />
+              </>
+            )
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,16 +1,18 @@
 import "./Navbar.css";
 import {
   FaBars,
+  FaCog,
   FaFacebook,
   FaInstagram,
   FaPinterest,
   FaSearch,
+  FaSignInAlt,
   FaSignOutAlt,
   FaTwitter,
 } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ User, setUser }) => {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
@@ -21,7 +23,7 @@ const Navbar = () => {
             </a>
           </div>
           <div>
-            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+            <ul className={User ? "navbar-nav me-3 mb-2 mb-lg-0" : "d-none"}>
               <li className="nav-item">
                 <Link
                   to="/:userName/profile"
@@ -57,17 +59,58 @@ const Navbar = () => {
             </ul>
           </div>
           <div className="d-flex align-items-center justfy-content-between">
+            <ul className={!User ? "navbar-nav me-3 mb-2 mb-lg-0" : "d-none"}>
+              <li className="nav-item">
+                <Link
+                  to="/"
+                  className="nav-link text-uppercase navbar-link-custome"
+                >
+                  home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="#about"
+                  className="nav-link text-uppercase navbar-link-custome"
+                >
+                  about
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  href="#contact"
+                  className="nav-link text-uppercase navbar-link-custome"
+                >
+                  contact
+                </a>
+              </li>
+            </ul>
             <Link to="/:userName/profile">
               <img
                 src="https://userstock.io/data/wp-content/uploads/2017/09/michael-dam-258165-300x300.jpg"
                 alt=""
-                className="img-fluid img-avator me-2"
+                className={!User ? "d-none" : "img-fluid img-avator me-3"}
               />{" "}
             </Link>
-            <FaSearch className="fs-4 me-2 icon-navbar-right" />
-            <Link to="/">
-              <FaSignOutAlt className="fs-4 me-2 icon-navbar-right" />{" "}
+            <FaSearch className="fs-4 me-3 icon-navbar-right" />
+            <Link to="/:userName/setting">
+              <FaCog
+                className={!User ? "d-none" : "fs-4 me-3 icon-navbar-right"}
+              />{" "}
             </Link>
+            {!User ? (
+              <Link to="/login">
+                <FaSignInAlt className="fs-4 me-3 icon-navbar-right " />
+              </Link>
+            ) : (
+              <Link to="/">
+                <FaSignOutAlt
+                  className="fs-4 me-3 icon-navbar-right"
+                  onClick={() => setUser(false)}
+                />
+              </Link>
+            )}
+
             <FaBars className="fs-4 d-md-none icon-navbar-right" />
           </div>
         </div>
